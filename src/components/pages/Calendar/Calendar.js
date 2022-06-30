fc632eb8fbd436f201451912a9e0183144c08f38
Shahriar Kasem <shahriarkasem@gmail.com>
2022-07-01 01:37:25 +0600

    calendar added

--- src/components/pages/Calendar/Calendar.js
+++ src/components/pages/Calendar/Calendar.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from "react";
+import CalendarMap from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const Calendar = () => {
-    return (
-        <section>
-            
-        </section>
-    );
+  const [date, setDate] = useState(new Date());
+  const [value, onChange] = useState(new Date());
+  return (
+    <section className="flex justify-center mt-10 md:mt-20">
+       <div>
+      <CalendarMap onChange={onChange} value={value} />
+    </div>
+    </section>
+  );
 };
 
 export default Calendar;
